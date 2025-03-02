8040e245a5cd44de34ef2cfd2b97e43920a73f2a
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-02 14:27:45 +0530

    create a perfect functional banner upload template

--- banner/views.py
+++ banner/views.py
@@ -1,20 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import bannerForm
-from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
-@csrf_exempt
 def bannerPage(request):
     form = bannerForm()
+    if request.method == "GET":
+        messages.info(request, "The price to upload a banner is $2")
     if request.method == 'POST':
         form = bannerForm(request.POST,  request.FILES)
         if form.is_valid():
-            messages.success( 
-                request, 'You need to pay for this banner upload'
-            )
             form.save()
-            return redirect('banner')
+            return redirect('home')
 
     context = {'form': form}
     return render(request, 'bannerUpload.html', context)

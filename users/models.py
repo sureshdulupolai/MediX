e91bbe2d4cfa9900f5d823690f6118e86b9aedd4
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-02 19:24:19 +0530

    created a profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ProfileDetails(models.Model):
+    Profile_Image = models.ImageField(upload_to='profile/')
+    Name = models.CharField(max_length=100)
+    Channel_Name = models.CharField(max_length=100)
+    Title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Name

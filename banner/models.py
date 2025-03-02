cd58f17bcc9cc7aa0d6f7a5c4cf6e9edd5bc1c6b
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-02 13:15:26 +0530

    Created a model in banner and model sent to admin panel

--- banner/models.py
+++ banner/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class BannerDetails(models.Model):
+    banner_img = models.ImageField(upload_to='banner/')
+    banner_title = models.CharField(max_length=200)
+    banner_datetime = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.banner_title

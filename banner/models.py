1e3678662a294b5ce5d8b06f1ee03cd8fc4c71b7
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-03 11:42:22 +0530

    Added a Feature of Direct Contact to Banner Uploaded User, in Whatapp

--- banner/models.py
+++ banner/models.py
@@ -6,6 +6,7 @@ class BannerDetails(models.Model):
     banner_img = models.ImageField(upload_to='banner/')
     banner_title = models.CharField(max_length=200)
     banner_datetime = models.DateTimeField(default=timezone.now)
+    contact_no = models.CharField(max_length=10, default='9820646838')
 
     def __str__(self):
         return self.banner_title

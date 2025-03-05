69bee1913156f2ef1b077a5e3338cb2153e04ca4
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-05 17:20:31 +0530

    Updated Banner Views

--- banner/views.py
+++ banner/views.py
@@ -5,14 +5,18 @@ from .forms import bannerForm
 
 # Create your views here.
 def bannerPage(request):
-    form = bannerForm()
-    if request.method == "GET":
-        messages.info(request, "The price to upload a banner is $2")
-    if request.method == 'POST':
-        form = bannerForm(request.POST,  request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+    if request.user.is_authenticated:
+        form = bannerForm()
+        if request.method == "GET":
+            messages.info(request, "The price to upload a banner is $2")
+        if request.method == 'POST':
+            form = bannerForm(request.POST,  request.FILES)
+            if form.is_valid():
+                form.save()
+                return redirect('home')
 
-    context = {'form': form}
-    return render(request, 'bannerUpload.html', context)
+        context = {'form': form}
+        return render(request, 'bannerUpload.html', context)
+    
+    else:
+        return redirect('login')

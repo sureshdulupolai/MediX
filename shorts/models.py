c5e06dda30edc810e5e8d241f01e459943f7615e
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-11 21:10:19 +0530

    updated a template of upload shorts and model

--- shorts/models.py
+++ shorts/models.py
@@ -7,7 +7,7 @@ class ShortsDetails(models.Model):
     short_thumbnail = models.ImageField(upload_to='thum/')
     short_title = models.CharField(max_length=100)
     short_link = models.FileField(upload_to='video/')
-    short_description = models.CharField(max_length=200)
+    short_description = models.TextField(max_length=200, null=True, blank=True)
     created_date = models.DateField(default=timezone.now)
     customer_name = models.ForeignKey(User, on_delete=models.CASCADE, default='user_name')
     view = models.FloatField(default=230.64)

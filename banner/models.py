51d33071a7a3c832842be79b1e6cb75c5f571e40
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-11 21:02:16 +0530

    updated a model of banner

--- banner/models.py
+++ banner/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class BannerDetails(models.Model):
@@ -7,6 +8,7 @@ class BannerDetails(models.Model):
     banner_title = models.CharField(max_length=200)
     banner_datetime = models.DateTimeField(default=timezone.now)
     contact_no = models.CharField(max_length=10)
+    uName = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.banner_title

b3aacf09383b122b7041cc4a6e2bc5be84a86721
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-18 12:13:47 +0530

    change a form of banner

--- banner/forms.py
+++ banner/forms.py
@@ -4,4 +4,4 @@ from .models import BannerDetails
 class bannerForm(forms.ModelForm):
     class Meta:
         model = BannerDetails
-        fields = ['banner_img', 'banner_title', 'contact_no']
+        fields = ['banner_img', 'banner_title', 'contact_no', 'uName']

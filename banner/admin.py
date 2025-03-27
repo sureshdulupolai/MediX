1c3a3ab2442deb40cf7db46e3edbbf50edcb1cec
Suresh Polai <sureshdulupolai@gmail.com>
2025-03-27 11:05:34 +0530

    update a admin panel for banner to which user upload a banner

--- banner/admin.py
+++ banner/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import BannerDetails
 
 class BannerAdmin(admin.ModelAdmin):
-    list_display = ['banner_title', 'banner_datetime']
+    list_display = ['banner_title', 'uName', 'banner_datetime']
 
 admin.site.register(BannerDetails, BannerAdmin)
